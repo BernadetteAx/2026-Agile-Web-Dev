a1b28062998815f3e106c5d1d0b576d35ac776aa
Sam Rose <samuelejrose@gmail.com>
2026-04-21 17:07:18 +0800

    Added all current HTML templates to the routes.py file and added requirements file for installation.

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,45 @@ main = Blueprint("main", __name__)
 
 
 # Format routes like this to add them to the Flask app
+
+
+#Route for the register page
+@main.route("/register")
+def register():
+    return render_template("register.html")
+
+# Route for the login page
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+# Route for the confirmation page after registration
+@main.route("/confirmation")
+def confirmation():
+    return render_template("confirm.html")
+
+# Routes for the home page 
 @main.route("/")
+@main.route("/home")
+def home():
+    return render_template("home.html")
+
+# Route for the daily hangman game page
+@main.route("/daily")
 def daily_hangman():
-    return render_template("daily-hangman.html")
+    return render_template("daily-page.html")
+
+# Route for the unlimited hangman game page
+@main.route("/unlimited")
+def unlimited_hangman():
+    return render_template("unlimited-page.html")
+
+# Route for the friends page
+@main.route("/friends")
+def friends():
+    return render_template("friend-page.html")
+
+# Route for the achievements page
+@main.route("/achievements")
+def achievements():
+    return render_template("achievements-page.html")
