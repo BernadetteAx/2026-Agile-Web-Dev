e330581e935c31de4ada95ec24dcc39dcccd9859
Sam Rose <samuelejrose@gmail.com>
2026-04-21 16:33:11 +0800

    Added requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def helloWorld():
-    return "Hello, World!"
+    from .routes import main
+    app.register_blueprint(main)
 
-if __name__ == "__main__":
-    app.run()
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, jsonify, request
-# Set of routes to be imported into the main app by __init__.py
+
+
+# Set of routes to be imported into the Flask app by __init__.py
 main = Blueprint("main", __name__)
 
 
+# Format routes like this to add them to the Flask app
 @main.route("/")
 def daily_hangman():
     return render_template("daily-hangman.html")
