4e9d10b232c337ef87a3a30ed6a19443e54c5cbe
Sam Rose <samuelejrose@gmail.com>
2026-05-08 13:20:08 +0800

    Prevent app running without secret key

--- app/config.py
+++ app/config.py
@@ -11,3 +11,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     # Set a secret key for the app. This is used to protect against CSRF attacks and to sign cookies.
     SECRET_KEY = os.getenv("HANGMAN_SECRET_KEY")
+    # Ensure that the SECRET_KEY environment variable is set. If not, raise an error to prevent the app from running without a secret key.
+    if not SECRET_KEY:
+        raise RuntimeError("SECRET_KEY environment variable is not set")
