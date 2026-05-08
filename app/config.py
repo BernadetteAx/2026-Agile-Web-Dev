df89e18a81a83de7d9f5d9e66eeb554b511c2db1
Sam Rose <samuelejrose@gmail.com>
2026-05-08 13:13:07 +0800

    Removed secret key from the code

--- app/config.py
+++ app/config.py
@@ -10,4 +10,4 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     # Set a secret key for the app. This is used to protect against CSRF attacks and to sign cookies.
-    SECRET_KEY = "they_could_have_taught_us_earlier"
+    SECRET_KEY = os.getenv("HANGMAN_SECRET_KEY")
